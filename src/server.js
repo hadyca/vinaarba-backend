97da4e830990298f8dac39ffc02844b0da83edbd
Jae Hyung Kim <SC425@ypsa.com>
2022-11-24 16:20:46 +0600

    헤로쿠

--- src/server.js
+++ src/server.js
@@ -2,18 +2,17 @@ require("dotenv").config();
 import http from "http";
 import express from "express";
 import logger from "morgan";
-// import { ApolloServer } from "apollo-server-express";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 async function startServer() {
   const apollo = new ApolloServer({
     typeDefs,
     resolvers,
-    // playground: true,
-    // introspection: true,
+    playground: true, // 추가
+    introspection: true, // 추가
     context: async ({ req }) => {
       return {
         loggedInUser: await getUser(req.headers.token),
@@ -21,15 +20,16 @@ async function startServer() {
     },
   });
 
-  // const app = express();
+  await apollo.start();
+  const app = express();
 
-  // app.use(logger("tiny"));
+  app.use(logger("tiny"));
   // app.use("/static", express.static("uploads"));
-  // apollo.applyMiddleware({ app });
-  // const httpServer = http.createServer(app);
-  apollo.listen(PORT, () => {
+  apollo.applyMiddleware({ app });
+  const httpServer = http.createServer(app);
+
+  httpServer.listen(PORT || 4000, () => {
     console.log(`🚀 Server is running http://localhost:${PORT}/graphql ✅`);
   });
 }
-
 startServer();
