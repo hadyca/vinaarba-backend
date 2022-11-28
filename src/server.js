b0e4862a962a0c307c082b78a7c13efb4406d766
Jae Hyung Kim <SC425@ypsa.com>
2022-11-28 11:03:43 +0600

    server파일일부수정

--- src/server.js
+++ src/server.js
@@ -2,35 +2,53 @@ require("dotenv").config();
 import http from "http";
 import express from "express";
 import logger from "morgan";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
 import { graphqlUploadExpress } from "graphql-upload";
 import { typeDefs, resolvers } from "./schema";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { getUser } from "./users/users.utils";
 const PORT = process.env.PORT;
 
 async function startServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
   const apollo = new ApolloServer({
     upload: false,
     typeDefs,
     resolvers,
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-      };
-    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    // context: async ({ req }) => {
+    //   return {
+    //     loggedInUser: await getUser(req.headers.token),
+    //   };
+    // },
   });
 
   await apollo.start();
-  const app = express();
-
+  app.use(
+    "/",
+    cors(),
+    bodyParser.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(apollo, {
+      context: async ({ req }) => {
+        return {
+          loggedInUser: await getUser(req.headers.token),
+        };
+      },
+    })
+  );
   app.use(logger("tiny"));
   app.use(graphqlUploadExpress({ maxFieldSize: 1000000 }));
-  apollo.applyMiddleware({ app });
-
-  const httpServer = http.createServer(app);
+  // apollo.applyMiddleware({ app });
 
   httpServer.listen(PORT || 4000, () => {
-    console.log(`🚀 Server is running http://localhost:${PORT}/graphql ✅`);
+    console.log(`🚀 Server is running http://localhost:${PORT}/`);
   });
 }
 startServer();
