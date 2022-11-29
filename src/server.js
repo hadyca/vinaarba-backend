b9cceb0f7d13cfeeb8ded0bff48ab124a30df604
unknown <hadycas@gmail.com>
2022-11-29 14:52:36 +0900

    베트남 언어 발신메일 수정

--- src/server.js
+++ src/server.js
@@ -2,12 +2,6 @@ require("dotenv").config();
 import http from "http";
 import express from "express";
 import logger from "morgan";
-<<<<<<< HEAD
-import { ApolloServer } from "apollo-server-express";
-import { graphqlUploadExpress } from "graphql-upload";
-import { typeDefs, resolvers } from "./schema";
-import { getUser } from "./users/users.utils";
-=======
 import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { expressMiddleware } from "@apollo/server/express4";
@@ -18,9 +12,7 @@ import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
->>>>>>> d57024952b19fcfa0aa63ba07214945d82192d03
 
-const PORT = process.env.PORT;
 async function startServer() {
   const app = express();
   const httpServer = http.createServer(app);
@@ -29,17 +21,9 @@ async function startServer() {
     upload: false,
     typeDefs,
     resolvers,
-<<<<<<< HEAD
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-      };
-    },
-=======
     playground: false,
     introspection: false,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
->>>>>>> d57024952b19fcfa0aa63ba07214945d82192d03
   });
 
   await apollo.start();
