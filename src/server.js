9ff8b551864e7bfe3f4013ac082a896b82319664
unknown <hadycas@gmail.com>
2022-11-29 21:49:20 +0600

    서버 수정3번째..

--- src/server.js
+++ src/server.js
@@ -2,47 +2,34 @@ require("dotenv").config();
 import http from "http";
 import express from "express";
 import logger from "morgan";
-import { ApolloServer } from "@apollo/server";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServer } from "apollo-server-express";
 import { graphqlUploadExpress } from "graphql-upload";
-import cors from "cors";
-import bodyParser from "body-parser";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
-
 async function startServer() {
-  const app = express();
-  const httpServer = http.createServer(app);
-
   const apollo = new ApolloServer({
-    upload: false,
     typeDefs,
     resolvers,
-    playground: false,
-    introspection: false,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: async ({ req }) => {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    },
   });
 
   await apollo.start();
-  app.use(
-    "/",
-    cors(),
-    bodyParser.json(),
-    expressMiddleware(apollo, {
-      context: async ({ req }) => {
-        return {
-          loggedInUser: await getUser(req.headers.token),
-        };
-      },
-    })
-  );
+  const app = express();
+
   app.use(logger("tiny"));
   app.use(graphqlUploadExpress({ maxFieldSize: 1000000 }));
+  apollo.applyMiddleware({ app, path: "/" });
+
+  const httpServer = http.createServer(app);
+
   httpServer.listen(PORT || 4000, () => {
-    console.log(`🚀 Server is running http://localhost:${PORT}/`);
+    console.log(`🚀 Server is running http://localhost:${PORT}/graphql ✅`);
   });
 }
 startServer();
